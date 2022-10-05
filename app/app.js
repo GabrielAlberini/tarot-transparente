76a2e8c5d90fec55c0a8ff49de86d3448c7bb1d7
Gabriel Alberini <gabialberini733@gmail.com>
2022-10-05 17:47:42 -0300

    Spinner en login y estilos de clases

--- app/app.js
+++ app/app.js
@@ -3,13 +3,20 @@ import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/fi
 
 let email = document.getElementById("email");
 let password = document.getElementById("password");
+let spinner = document.getElementById("spinner");
 
 let btn = document.getElementById("register").addEventListener("click", signinUser);
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
 
 function signinUser() {
     signInWithEmailAndPassword(auth, email.value, password.value)
     .then(() => {
-        window.location.href = "/views/login/index.html";
+      spinner.classList.toggle("spinner");
+      window.location.href = "/views/login/index.html";
     })
     .catch((error) => {
       console.log(error);
